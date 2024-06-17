114605f5a1dff5248e029df055b1375b29671671
Daniel Baker <daniel.n.baker@gmail.com>
2024-06-16 20:39:40 -0700

    Forgot to give gildenfire a home config.

---
+++ home-configurations/gildenfire/djacu/default.nix
@@ -0,0 +1,13 @@
+{
+  system = "x86_64-linux";
+  release = "2405";
+  modules = [
+    {
+      home.stateVersion = "24.05";
+
+      theonecfg.users.djacu.enable = true;
+      theonecfg.users.djacu.dev.enable = true;
+      #theonecfg.users.djacu.desktop.enable = true;
+    }
+  ];
+}
