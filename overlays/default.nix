8953563c0cb0cfba19c4dfbd186191a763151c15
Daniel Baker <daniel.n.baker@gmail.com>
2024-07-03 21:29:27 -0700

    overlays: add my own version of age that has age-plugin-yubikey on the path

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,1 +1,21 @@
-inputs: { default = inputs.nixpkgs-unstable.lib.composeManyExtensions [ inputs.nur.overlay ]; }
+inputs: {
+  default = inputs.nixpkgs-unstable.lib.composeManyExtensions [
+
+    inputs.nur.overlay
+
+    # Create my own modified age so that age-plugin-yubikey is on the path.
+    (final: _: {
+      theonecfg.age = final.age.overrideAttrs (prevAttrs: {
+        nativeBuildInputs = (prevAttrs.nativeBuildInputs or [ ]) + [ final.makeWrapper ];
+
+        postInstall =
+          (prevAttrs.postInstall or "")
+          + ''
+            wrapProgram $out/bin/age \
+              --prefix PATH : ${final.lib.makeBinPath [ final.age-plugin-yubikey ]}
+          '';
+      });
+    })
+
+  ];
+}
