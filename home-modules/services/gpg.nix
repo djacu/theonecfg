2d455655cf43980a4708b7d39a619c6f50ba6430
Daniel Baker <daniel.n.baker@gmail.com>
2024-09-07 06:32:10 -0700

    home-modules: services: init gpg
    Create a gpg/gpg-agent service module.

---
+++ home-modules/services/gpg.nix
@@ -0,0 +1,17 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+let
+  cfg = config.theonecfg.home.services.gpg;
+in
+{
+  options.theonecfg.home.services.gpg.enable = lib.mkEnableOption "gpg config";
+
+  config = lib.mkIf cfg.enable {
+    services.gpg-agent.enable = true;
+    services.gpg-agent.pinentryPackage = pkgs.pinentry-tty;
+  };
+}
