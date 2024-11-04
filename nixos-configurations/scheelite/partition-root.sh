dd67d3be91e675868c10fb599c4372221e73ee8c
Daniel Baker <daniel.n.baker@gmail.com>
2024-11-03 19:06:57 -0800

    nixos-configurations.scheelite: init
    Add partition scripts for scheelite host.

---
+++ nixos-configurations/scheelite/partition-root.sh
@@ -0,0 +1,95 @@
+#!/bin/env bash
+
+DISK=(
+  "/dev/disk/by-id/nvme-Samsung_SSD_990_PRO_2TB_S7KHNJ0X208893V"
+  "/dev/disk/by-id/nvme-Samsung_SSD_990_PRO_2TB_S7KHNJ0X208806D"
+)
+EFISIZE=10
+SWAPSIZE=64
+RESERVE=0
+POOLNAME="scheelite-root"
+
+partition_disk() {
+  local disk="${1}"
+  blkdiscard -f "${disk}" || true
+
+  parted --script --align=optimal "${disk}" -- \
+    mklabel gpt \
+    mkpart EFI 1MiB $((EFISIZE))GiB \
+    mkpart "${POOLNAME}" $((EFISIZE))GiB -$((SWAPSIZE + RESERVE))GiB \
+    mkpart SWAP -$((SWAPSIZE + RESERVE))GiB -"${RESERVE}"GiB \
+    set 1 esp on
+
+  partprobe "${disk}"
+}
+
+for i in "${DISK[@]}"; do
+  echo "${i}"
+  partition_disk "${i}"
+done
+
+for i in "${DISK[@]}"; do
+  mkswap "${i}-part3"
+  swapon "${i}-part3"
+done
+
+# shellcheck disable=SC2046
+zpool create \
+  -o ashift=12 \
+  -o autoexpand=on \
+  -o autotrim=on \
+  -R "${MNT}" \
+  -O acltype=posix \
+  -O atime=off \
+  -O canmount=off \
+  -O checksum=fletcher4 \
+  -O compression=lz4 \
+  -O dnodesize=auto \
+  -O relatime=on \
+  -O xattr=sa \
+  -O mountpoint=none \
+  "${POOLNAME}" \
+  mirror \
+  $(for i in "${DISK[@]}"; do
+    printf '%s ' "${i}-part2"
+  done)
+
+zfs create \
+  -o canmount=off \
+  "${POOLNAME}"/local
+
+zfs create \
+  -o mountpoint=/ \
+  "${POOLNAME}"/local/root
+
+zfs snapshot "${POOLNAME}"/local/root@empty
+
+zfs create \
+  -o mountpoint=/nix \
+  "${POOLNAME}"/local/nix
+
+zfs create \
+  -o canmount=off \
+  "${POOLNAME}"/safe
+
+zfs create \
+  -o mountpoint=/home \
+  "${POOLNAME}"/safe/home
+
+zfs create \
+  -o mountpoint=/persist \
+  "${POOLNAME}"/safe/persist
+
+mount -o X-mount.mkdir -t zfs "${POOLNAME}"/local/root "${MNT}"
+mount -o X-mount.mkdir -t zfs "${POOLNAME}"/local/nix "${MNT}"/nix
+mount -o X-mount.mkdir -t zfs "${POOLNAME}"/safe/home "${MNT}"/home
+mount -o X-mount.mkdir -t zfs "${POOLNAME}"/safe/persist "${MNT}"/persist
+
+for i in "${DISK[@]}"; do
+  mkfs.vfat -n EFI "${i}"-part1
+done
+
+for i in "${DISK[@]}"; do
+  mount -t vfat -o fmask=0077,dmask=0077,iocharset=iso8859-1,X-mount.mkdir "${i}"-part1 "${MNT}"/boot
+  break
+done
---
+++ nixos-configurations/scheelite/partition-tank.sh
@@ -0,0 +1,76 @@
+#!/bin/env bash
+
+DISK=(
+  "/dev/disk/by-id/wwn-0x5000cca2902b7288"
+  "/dev/disk/by-id/wwn-0x5000cca2902bcf64"
+  "/dev/disk/by-id/wwn-0x5000cca2902be164"
+  "/dev/disk/by-id/wwn-0x5000cca2902c39f4"
+  "/dev/disk/by-id/wwn-0x5000cca2902c3a78"
+  "/dev/disk/by-id/wwn-0x5000cca2902c3b14"
+  "/dev/disk/by-id/wwn-0x5000cca2902c6ed0"
+  "/dev/disk/by-id/wwn-0x5000cca2902c71c8"
+)
+POOLNAME="scheelite-tank0"
+
+partition_disk() {
+  local disk="${1}"
+  blkdiscard -f "${disk}" || true
+
+  parted --script --align=optimal "${disk}" -- \
+    mklabel gpt \
+    mkpart "${POOLNAME}" 0% 100%
+
+  partprobe "${disk}"
+}
+
+for i in "${DISK[@]}"; do
+  partition_disk "${i}"
+done
+
+# shellcheck disable=SC2046
+zpool create \
+  -o ashift=12 \
+  -o autoexpand=on \
+  -R "${MNT}" \
+  -O acltype=posix \
+  -O atime=off \
+  -O canmount=off \
+  -O checksum=fletcher4 \
+  -O compression=lz4 \
+  -O dnodesize=auto \
+  -O relatime=on \
+  -O xattr=sa \
+  -O mountpoint=none \
+  "${POOLNAME}" \
+  mirror \
+  $(for i in "${DISK[@]}"; do
+    printf '%s ' "${i}-part1"
+  done)
+
+zfs create \
+  -o mountpoint=/tank0 \
+  "${POOLNAME}"/tank0
+
+zfs create \
+  -o recordsize=1M \
+  -o mountpoint=/tank0/video \
+  "${POOLNAME}"/tank0/video
+
+zfs create \
+  -o recordsize=1M \
+  -o mountpoint=/tank0/audio \
+  "${POOLNAME}"/tank0/audio
+
+zfs create \
+  -o recordsize=1M \
+  -o mountpoint=/tank0/images \
+  "${POOLNAME}"/tank0/images
+
+zfs create \
+  -o mountpoint=/tank0/bulk \
+  "${POOLNAME}"/tank0/bulk
+
+mount -o X-mount.mkdir -t zfs "${POOLNAME}"/tank0/audio "${MNT}/tank0/audio"
+mount -o X-mount.mkdir -t zfs "${POOLNAME}"/tank0/bulk "${MNT}/tank0/bulk"
+mount -o X-mount.mkdir -t zfs "${POOLNAME}"/tank0/images "${MNT}/tank0/images"
+mount -o X-mount.mkdir -t zfs "${POOLNAME}"/tank0/video "${MNT}/tank0/video"
