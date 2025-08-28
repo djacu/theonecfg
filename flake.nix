eab383e269c0e231be7c2a96a9281a556ebe8de3
Daniel Baker <daniel.n.baker@gmail.com>
2025-08-28 06:12:51 -0700

    flake-inputs: add nixpkgs 25.05, add home-manager 25.05, add nixpkgs-lib

--- flake.nix
+++ flake.nix
@@ -6,9 +6,13 @@
     home-manager-2405.inputs.nixpkgs.follows = "nixpkgs-2405";
     home-manager-2411.url = "github:nix-community/home-manager/release-24.11";
     home-manager-2411.inputs.nixpkgs.follows = "nixpkgs-2411";
+    home-manager-2505.url = "github:nix-community/home-manager/release-25.05";
+    home-manager-2505.inputs.nixpkgs.follows = "nixpkgs-2505";
     impermanence.url = "github:nix-community/impermanence";
+    nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
     nixpkgs-2405.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-2411.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-2505.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixos-hardware.url = "github:nixos/nixos-hardware";
     nixvimcfg.url = "github:djacu/nixvimcfg";
