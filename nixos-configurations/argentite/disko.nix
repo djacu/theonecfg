f482bf39304b7212ee6c3b5b9db28d581148aae1
Daniel Baker <daniel.n.baker@gmail.com>
2025-10-13 19:48:42 -0700

    nixos-configurations.argentite: init

---
+++ nixos-configurations/argentite/disko.nix
@@ -0,0 +1,102 @@
+{
+  disko.devices = {
+    disk = {
+      disk1 = {
+        type = "disk";
+        device = "/dev/disk/by-id/nvme-Samsung_SSD_970_PRO_1TB_S462NF0K816201E";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "4G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            # swap = {
+            #   size = "32G";
+            #   type = "8200";
+            #   content = {
+            #     type = "swap";
+            #     resumeDevice = true; # resume from hiberation from this device
+            #   };
+            # };
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
+            };
+          };
+        };
+      };
+    };
+    zpool = {
+      zroot = {
+        type = "zpool";
+        rootFsOptions = {
+          acltype = "posixacl";
+          canmount = "off";
+          checksum = "edonr";
+          compression = "lz4";
+          dnodesize = "auto";
+          # encryption does not appear to work in vm test; only use on real system
+          encryption = "aes-256-gcm";
+          keyformat = "passphrase";
+          keylocation = "prompt";
+          normalization = "formD";
+          relatime = "on";
+          xattr = "sa";
+        };
+        mountpoint = null;
+        options = {
+          ashift = "12";
+          autotrim = "on";
+        };
+
+        datasets = {
+          local = {
+            type = "zfs_fs";
+            options.canmount = "off";
+          };
+
+          safe = {
+            type = "zfs_fs";
+            options.canmount = "off";
+          };
+
+          "local/root" = {
+            type = "zfs_fs";
+            mountpoint = "/";
+            options.mountpoint = "/";
+            postCreateHook = ''
+              zfs snapshot zroot/local/root@empty
+            '';
+          };
+
+          "local/nix" = {
+            type = "zfs_fs";
+            mountpoint = "/nix";
+            options.mountpoint = "/nix";
+          };
+
+          "safe/home" = {
+            type = "zfs_fs";
+            mountpoint = "/home";
+            options.mountpoint = "/home";
+          };
+
+          "safe/persist" = {
+            type = "zfs_fs";
+            mountpoint = "/persist";
+            options.mountpoint = "/persist";
+          };
+        };
+      };
+    };
+  };
+}
