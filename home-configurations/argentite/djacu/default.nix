51ae8761abf8b87ef130575906619081d4a0c1eb
Daniel Baker <daniel.n.baker@gmail.com>
2025-10-13 19:50:35 -0700

    home-configurations.argentite.djacu: init

---
+++ home-configurations/argentite/djacu/default.nix
@@ -0,0 +1,15 @@
+{
+  system = "x86_64-linux";
+  release = "2505";
+  modules = [
+    {
+      home.stateVersion = "25.05";
+
+      theonecfg.users.djacu.enable = true;
+      theonecfg.users.djacu.dev.enable = true;
+      theonecfg.users.djacu.desktop.enable = true;
+
+      theonecfg.home.programs.messaging.enable = true;
+    }
+  ];
+}
