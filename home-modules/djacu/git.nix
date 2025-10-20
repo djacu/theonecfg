a43e7e16e5232b3c0ebb667a51b6fee833047122
Daniel Baker <dan@djacu.dev>
2025-10-19 20:04:09 -0700

    home-modules.djacu.git: setup git with the gpg key on the yubikey

--- home-modules/djacu/git.nix
+++ home-modules/djacu/git.nix
@@ -30,6 +30,27 @@ in
         init.defaultBranch = "main";
         merge.conflictstyle = "zdiff3";
         rerere.enabled = true;
+
+        # gpg
+        commit.gpgsign = true;
+        tag.gpgSign = true;
+        /*
+          To see all keys
+            gpg --list-keys
+
+          To see just the line with the key needed for signing
+            gpg --list-keys | awk '/pub   /'
+
+          To see just the key
+            gpg --list-keys | awk '/^pub[[:space:]]/ { split($2,a,"/"); print a[2] }'
+
+          To get the ssh key from that
+            gpg --list-keys \
+            | awk '/^pub[[:space:]]/ { split($2,a,"/"); print a[2] }' \
+            | xargs -r gpg --export-ssh-key
+        */
+        user.signingkey = "0x8C9CFEF8EE37DA95";
+        gpg.format = "openpgp";
       };
     };
   };
