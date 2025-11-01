80796ad47bf678282ec454195be049671a32c5e4
Daniel Baker <dan@djacu.dev>
2025-10-31 21:10:44 -0700

    home-modules.djacu.dev: setup ssh config

--- home-modules/djacu/dev.nix
+++ home-modules/djacu/dev.nix
@@ -19,6 +19,23 @@ in
     theonecfg.home.programs.tmux.enable = true;
 
     programs.ssh.enable = true;
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.ssh.enableDefaultConfig
+    # programs.ssh.matchBlocks."*" = {
+    #   forwardAgent = false;
+    #   addKeysToAgent = "no";
+    #   compression = false;
+    #   serverAliveInterval = 0;
+    #   serverAliveCountMax = 3;
+    #   hashKnownHosts = false;
+    #   userKnownHostsFile = "~/.ssh/known_hosts";
+    #   controlMaster = "no";
+    #   controlPath = "~/.ssh/master-%r@%n:%p";
+    #   controlPersist = "no";
+    # };
+    programs.ssh.matchBlocks.argentite = {
+      hostname = "argentite";
+      forwardAgent = true;
+    };
 
     home.packages = with pkgs; [
 
