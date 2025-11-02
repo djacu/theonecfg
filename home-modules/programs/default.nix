c7d7dc0081f307e0f2800ee4a9202e160efb6143
Daniel Baker <dan@djacu.dev>
2025-11-01 22:14:21 -0700

    home-modules.programs: automate imports

--- home-modules/programs/default.nix
+++ home-modules/programs/default.nix
@@ -1,11 +1,29 @@
-{ ... }:
 {
-  imports = [
-    ./fd.nix
-    ./fish.nix
-    ./kitty.nix
-    ./messaging.nix
-    ./nixvimcfg.nix
-    ./tmux.nix
-  ];
+  lib,
+  ...
+}:
+let
+
+  inherit (builtins)
+    readDir
+    ;
+
+  inherit (lib.attrsets)
+    attrNames
+    filterAttrs
+    ;
+
+  inherit (lib.lists)
+    map
+    ;
+
+  inherit (lib.trivial)
+    const
+    ;
+
+in
+{
+  imports = map (directory: ./${directory}/module.nix) (
+    attrNames (filterAttrs (const (filetype: filetype == "directory")) (readDir ./.))
+  );
 }
