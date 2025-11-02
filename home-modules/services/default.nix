820a43fedbd7a23dd68db06dd11d0fb21846b449
Daniel Baker <dan@djacu.dev>
2025-11-01 22:15:27 -0700

    home-modules.services: automate imports

--- home-modules/services/default.nix
+++ home-modules/services/default.nix
@@ -1,4 +1,29 @@
-{ ... }:
 {
-  imports = [ ];
+  lib,
+  ...
+}:
+let
+
+  inherit (builtins)
+    readDir
+    ;
+
+  inherit (lib.attrsets)
+    attrNames
+    filterAttrs
+    ;
+
+  inherit (lib.lists)
+    map
+    ;
+
+  inherit (lib.trivial)
+    const
+    ;
+
+in
+{
+  imports = map (directory: ./${directory}/module.nix) (
+    attrNames (filterAttrs (const (filetype: filetype == "directory")) (readDir ./.))
+  );
 }
