4e0d76fdb074657565466c20ae63488abc5f03ac
Daniel Baker <dan@djacu.dev>
2025-11-03 21:30:13 -0800

    nixos-configurations: use new library functions

--- nixos-configurations/default.nix
+++ nixos-configurations/default.nix
@@ -8,37 +8,31 @@ let
   ];
 in
 inputs:
-builtins.mapAttrs
-  (
-    hostDirectory: _:
-    let
-      inherit (import ./${hostDirectory}) release modules;
-    in
-    inputs."nixpkgs-${release}".lib.nixosSystem {
-      modules = [
-        (
-          { config, ... }:
-          {
-            assertions = [
-              {
-                assertion = builtins.elem config.networking.hostName knownConfigurations;
-                message = "Hostname is not known!";
-              }
-            ];
-            networking.hostName = hostDirectory;
-          }
-        )
-        inputs.self.nixosModules.default
-        modules
-      ];
+builtins.mapAttrs (
+  hostDirectory: _:
+  let
+    inherit (import ./${hostDirectory}) release modules;
+  in
+  inputs."nixpkgs-${release}".lib.nixosSystem {
+    modules = [
+      (
+        { config, ... }:
+        {
+          assertions = [
+            {
+              assertion = builtins.elem config.networking.hostName knownConfigurations;
+              message = "Hostname is not known!";
+            }
+          ];
+          networking.hostName = hostDirectory;
+        }
+      )
+      inputs.self.nixosModules.default
+      modules
+    ];
 
-      specialArgs = {
-        inherit inputs;
-      };
-    }
-  )
-  (
-    inputs.nixpkgs-unstable.lib.attrsets.filterAttrs (_: fileType: fileType == "directory") (
-      builtins.readDir ./.
-    )
-  )
+    specialArgs = {
+      inherit inputs;
+    };
+  }
+) (inputs.self.library.path.getDirectories ./.)
