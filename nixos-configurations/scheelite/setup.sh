33f6a7f6cc90294e230d16ead758f483a0c87853
Daniel Baker <dan@djacu.dev>
2025-11-03 19:14:38 -0800

    nixos-configurations.scheelite: make shellcheck happy

--- nixos-configurations/scheelite/setup.sh
+++ nixos-configurations/scheelite/setup.sh
@@ -5,6 +5,9 @@ set -u
 set -o pipefail
 set -x
 
+# shellcheck disable=SC2034
 MNT=$(mktemp -d)
+# shellcheck disable=SC1091
 source ./partition-root.sh
+# shellcheck disable=SC1091
 source ./partition-tank.sh
