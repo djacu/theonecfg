bf41fd603ff2dd1f1914d6659f5add84d9b46139
Daniel Baker <dan@djacu.dev>
2025-11-05 18:15:11 -0800

    home-configurations: improve home configuration generation

--- home-configurations/default.nix
+++ home-configurations/default.nix
@@ -1,36 +1,51 @@
 inputs:
 let
-  inherit (inputs.nixpkgs-unstable.lib)
-    flatten
-    filterAttrs
-    mapAttrsToList
-    nameValuePair
+
+  inherit (inputs.nixpkgs-lib)
+    lib
     ;
 
-  inherit (builtins) listToAttrs readDir;
-in
-listToAttrs (
-  flatten (
-    mapAttrsToList (
-      hostName: _:
-      mapAttrsToList (
-        userName: _:
+  inherit (lib.attrsets)
+    genAttrs'
+    mergeAttrsList
+    ;
+
+  inherit (lib.lists)
+    map
+    ;
+
+  inherit (lib.trivial)
+    pipe
+    ;
+
+  inherit (inputs.self.library.path)
+    getDirectoryNames
+    ;
+
+  mkUsersForHost =
+    host:
+    genAttrs' (getDirectoryNames ./${host}) (user: {
+      name = host + "-" + user;
+      value =
         let
-          inherit (import ./${hostName}/${userName}) system modules release;
+          inherit (import ./${host}/${user}) system modules release;
         in
-        nameValuePair "${hostName}-${userName}" (
-          inputs."home-manager-${release}".lib.homeManagerConfiguration {
-            pkgs = import inputs."nixpkgs-${release}" {
-              inherit system;
-              overlays = [ inputs.self.overlays.default ];
-            };
-            modules = [ inputs.self.homeModules.${userName} ] ++ modules;
-            extraSpecialArgs = {
-              inherit inputs system;
-            };
-          }
-        )
-      ) (filterAttrs (_: fileType: fileType == "directory") (readDir ./${hostName}))
-    ) (filterAttrs (_: fileType: fileType == "directory") (readDir ./.))
-  )
-)
+        inputs."home-manager-${release}".lib.homeManagerConfiguration {
+          pkgs = import inputs."nixpkgs-${release}" {
+            inherit system;
+            overlays = [ inputs.self.overlays.default ];
+          };
+          modules = [ inputs.self.homeModules.${user} ] ++ modules;
+          extraSpecialArgs = {
+            inherit inputs system;
+          };
+        };
+    });
+
+in
+
+pipe ./. [
+  getDirectoryNames
+  (map mkUsersForHost)
+  mergeAttrsList
+]
