6870cbc65e084b74f7bf09d8723ac2e854c5e824
Daniel Baker <dan@djacu.dev>
2025-11-07 21:04:05 -0800

    home-modules.djacu: move productivity from dev to desktop

--- home-modules/djacu/desktop/module.nix
+++ home-modules/djacu/desktop/module.nix
@@ -6,9 +6,13 @@ in
   options.theonecfg.users.djacu.desktop.enable = lib.mkEnableOption "djacu desktop config";
 
   config = lib.mkIf (cfg.enable && cfg.desktop.enable) {
+
     theonecfg.users.djacu.firefox.enable = true;
     theonecfg.users.djacu.gpg.enable = true;
 
     theonecfg.home.programs.kitty.enable = true;
+
+    theonecfg.packages.productivity.enable = true;
+
   };
 }
--- home-modules/djacu/dev/module.nix
+++ home-modules/djacu/dev/module.nix
@@ -10,6 +10,7 @@ in
   options.theonecfg.users.djacu.dev.enable = lib.mkEnableOption "djacu dev config";
 
   config = lib.mkIf (cfg.enable && cfg.dev.enable) {
+
     theonecfg.users.djacu.git.enable = true;
     theonecfg.users.djacu.fish.enable = false;
 
@@ -22,7 +23,6 @@ in
     theonecfg.packages.developer.enable = true;
     theonecfg.packages.networking.enable = true;
     theonecfg.packages.nix.enable = true;
-    theonecfg.packages.productivity.enable = true;
 
   };
 }
