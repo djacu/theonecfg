764bc0a5d88e70ed4b9a36a9ed1e95df1e259bfc
Daniel Baker <dan@djacu.dev>
2025-11-11 23:21:30 -0800

    *-configurations.cassiterite: bump 24.11 -> 25.05

--- nixos-configurations/cassiterite/default.nix
+++ nixos-configurations/cassiterite/default.nix
@@ -1,8 +1,7 @@
 {
-  release = "2411";
+  release = "2505";
   modules =
     {
-      config,
       pkgs,
       theonecfg,
       ...
@@ -19,7 +18,7 @@
         nixpkgs.hostPlatform = "x86_64-linux";
         system.stateVersion = "24.05";
 
-        boot.kernelPackages = theonecfg.library.zfs.latestKernelPackage { inherit config pkgs; };
+        boot.kernelPackages = pkgs.linuxPackages_6_15;
         boot.loader.systemd-boot.enable = true;
         boot.loader.efi.canTouchEfiVariables = true;
         boot.loader.efi.efiSysMountPoint = "/boot";
@@ -42,9 +41,8 @@
         users.mutableUsers = false;
         users.users.root.initialHashedPassword = "$6$efX.JpKjAey2jrYG$kOt..AuFrPPIVTDncVj7vNkIo4MR/9mYG2SaDV2xpSNDEmk8DRxVNmuMI6hcW.CmD6ZDqdIKCj2MAyHnIdrkl/";
 
-        theonecfg.common.enable = true;
-        theonecfg.basicNetwork.enable = true;
-        theonecfg.desktop.enable = true;
+        theonecfg.profiles.common.enable = true;
+        theonecfg.profiles.desktop.enable = true;
 
         theonecfg.users.djacu.enable = true;
         users.users.djacu.initialHashedPassword = "$6$XfxMt044Az$jyfPvBMkcpPN.s9ZiVQyBjnU5g3x2cFZjBO3okgTENI2YjP6XokJr7QIaREPIcXiS4z5N9HnavEJEk.tneT670";
