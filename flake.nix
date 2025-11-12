dc82eaf69ef6373163c3e044ac3ff3b235531069
Daniel Baker <dan@djacu.dev>
2025-11-11 23:17:08 -0800

    flake-inputs: remove references to 24.05 release

--- flake.nix
+++ flake.nix
@@ -2,15 +2,12 @@
   inputs = {
     disko.url = "github:nix-community/disko/";
     disko.inputs.nixpkgs.follows = "nixpkgs-unstable";
-    home-manager-2405.url = "github:nix-community/home-manager/release-24.05";
-    home-manager-2405.inputs.nixpkgs.follows = "nixpkgs-2405";
     home-manager-2411.url = "github:nix-community/home-manager/release-24.11";
     home-manager-2411.inputs.nixpkgs.follows = "nixpkgs-2411";
     home-manager-2505.url = "github:nix-community/home-manager/release-25.05";
     home-manager-2505.inputs.nixpkgs.follows = "nixpkgs-2505";
     impermanence.url = "github:nix-community/impermanence";
     nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
-    nixpkgs-2405.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-2411.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-2505.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
