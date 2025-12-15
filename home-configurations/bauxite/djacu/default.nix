f119648be5e130633549826fea727bbddca8f43b
Daniel Baker <dan@djacu.dev>
2025-12-14 18:17:03 -0800

    home-configurations.bauxite.djacu: init

---
+++ home-configurations/bauxite/djacu/default.nix
@@ -0,0 +1,21 @@
+inputs: {
+  system = "aarch64-linux";
+  release = rec {
+    number = "2505";
+    nixpkgs = inputs."nixpkgs-${number}";
+    home-manager = inputs."home-manager-${number}";
+  };
+  modules = [
+    {
+
+      home.stateVersion = "25.05";
+
+      theonecfg.users.djacu.enable = true;
+
+      theonecfg.users.djacu.profiles.common.enable = true;
+      theonecfg.users.djacu.profiles.desktop.enable = true;
+      theonecfg.users.djacu.profiles.developer.enable = true;
+
+    }
+  ];
+}
