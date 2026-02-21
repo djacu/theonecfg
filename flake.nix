80739f122a76d3d5c8fa6fdc2d1850ab3a9a19dc
Daniel Baker <dan@djacu.dev>
2026-02-21 13:03:39 -0800

    flake.inputs: home manager unstable add

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
     disko.inputs.nixpkgs.follows = "nixpkgs-unstable";
     home-manager-2511.url = "github:nix-community/home-manager/release-25.11";
     home-manager-2511.inputs.nixpkgs.follows = "nixpkgs-2511";
+    home-manager-unstable.url = "github:nix-community/home-manager";
+    home-manager-unstable.inputs.nixpkgs.follows = "nixpkgs-unstable";
     impermanence.url = "github:nix-community/impermanence";
     nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
     nixpkgs-2511.url = "github:nixos/nixpkgs/nixos-25.11";
