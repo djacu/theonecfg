938b3c88c7e6afa239e8217ffbf192f43cc56532
Daniel Baker <dan@djacu.dev>
2026-02-21 17:35:11 -0800

    homeModules.users.djacu.programs.gpg: don't use default ssh config
    Fixes:
    trace: warning: `programs.ssh` default values will be removed in the future.
    Consider setting `programs.ssh.enableDefaultConfig` to false,
    and manually set the default values you want to keep at
    `programs.ssh.matchBlocks."*"`.

--- home-modules/users/djacu/programs/gpg/module.nix
+++ home-modules/users/djacu/programs/gpg/module.nix
@@ -138,19 +138,22 @@ in
 
       programs.ssh.enable = true;
       # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.ssh.enableDefaultConfig
-      # programs.ssh.matchBlocks."*" = {
-      #   forwardAgent = false;
-      #   addKeysToAgent = "no";
-      #   compression = false;
-      #   serverAliveInterval = 0;
-      #   serverAliveCountMax = 3;
-      #   hashKnownHosts = false;
-      #   userKnownHostsFile = "~/.ssh/known_hosts";
-      #   controlMaster = "no";
-      #   controlPath = "~/.ssh/master-%r@%n:%p";
-      #   controlPersist = "no";
-      # };
-      programs.ssh.matchBlocks = mkHostMatchBlock forwardedHosts;
+      programs.ssh.enableDefaultConfig = false;
+      programs.ssh.matchBlocks = {
+        "*" = {
+          forwardAgent = false;
+          addKeysToAgent = "no";
+          compression = false;
+          serverAliveInterval = 0;
+          serverAliveCountMax = 3;
+          hashKnownHosts = false;
+          userKnownHostsFile = "~/.ssh/known_hosts";
+          controlMaster = "no";
+          controlPath = "~/.ssh/master-%r@%n:%p";
+          controlPersist = "no";
+        };
+      }
+      // (mkHostMatchBlock forwardedHosts);
 
     })
 
