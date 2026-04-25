781d9ed6ec1945c0475b4cdbcb0cf935f7b84133
Daniel Baker <dan@djacu.dev>
2026-04-25 13:32:00 -0700

    homeModules.theonecfg.users.djacu.profiles.developer: enable claude

--- home-modules/users/djacu/profiles/developer/module.nix
+++ home-modules/users/djacu/profiles/developer/module.nix
@@ -30,6 +30,7 @@ in
     theonecfg.programs.zellij.enable = true;
 
     theonecfg.packages.admin.enable = true;
+    theonecfg.packages.claude.enable = true;
     theonecfg.packages.developer.enable = true;
     theonecfg.packages.networking.enable = true;
     theonecfg.packages.nix.enable = true;
