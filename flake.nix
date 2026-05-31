eb90af4d6771d03e68db3d5b39e3527b368cb32d
Daniel Baker <dan@djacu.dev>
2026-05-31 15:08:43 -0700

    flakes: remove 25.11 pins

--- flake.nix
+++ flake.nix
@@ -2,13 +2,10 @@
   inputs = {
     disko.url = "github:nix-community/disko/";
     disko.inputs.nixpkgs.follows = "nixpkgs-unstable";
-    home-manager-2511.url = "github:nix-community/home-manager/release-25.11";
-    home-manager-2511.inputs.nixpkgs.follows = "nixpkgs-2511";
     home-manager-unstable.url = "github:nix-community/home-manager";
     home-manager-unstable.inputs.nixpkgs.follows = "nixpkgs-unstable";
     impermanence.url = "github:nix-community/impermanence";
     nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
-    nixpkgs-2511.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:nixos/nixos-hardware";
     nixvimcfg.url = "github:djacu/nixvimcfg";
